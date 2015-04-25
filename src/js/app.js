bcf01bd0b9613008abbf6b9d42ba5cf8ae678255
Constantine Karnaukhov <genteelknight@gmail.com>
2015-04-25 22:33:10 +0400

    Update authentication, start develop pagination

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,39 @@
-// Init Authentication
-Auth();
+/*
+* Authentication
+*   localStorage.authStep
+*   0 - need auth
+*   1 - get access token
+*   2 - access token in localStorage
+* */
+// Define session information in localStorage
+localStorage.clientId = "c5004e18b7474d4cb26c1f76af8ab98e";
+
+// Check was user authorized
+if (localStorage.authStep != 1 && localStorage.authStep != 2) {
+  localStorage.authStep = 0;
+}
+
+if (localStorage.authStep == 1) {
+  // Parse access token
+  var url = window.location.href,
+    slice = "access_token=",
+    index = url.indexOf(slice);
+
+  if (index != -1) {
+    // Get access token
+    localStorage.accessToken = url.substring(index + slice.length);
+    // Update authStep
+    localStorage.authStep = 2;
+    console.log('Auth success');
+  } else {
+    // Reset authStep
+    localStorage.authStep = 0;
+    console.log("You need auth");
+  }
+}
+
+if (localStorage.authStep == 2)
+  console.log('Already authorized');
 
 // Init Angular
 var app = angular.module('selfieprint', [ ]);
@@ -24,22 +58,54 @@ function MainCTRL($scope, $http) {
     $scope.showExit = false;
   }
 
-  // Call Exit function
+  /*
+  * Exit
+  * Clear access token and reset authStep on exit
+  * */
   $scope.Exit = function () {
-    Exit();
+    localStorage.accessToken = "";
+    localStorage.authStep = 0;
+    location.reload();
   };
 
   // Include Page
-  if (localStorage.authStep != 0)
+  if (localStorage.authStep == 2)
     $scope.page = $scope.includes.app;
   else
     $scope.page = $scope.includes.landing;
 
+  main.login = function () {
+    // Update authStep
+    localStorage.authStep = 1;
+    // Redirect to Instagram auth page
+    window.location.href = "https://instagram.com/oauth/authorize/?client_id="+ localStorage.clientId +"&redirect_uri="+ window.location.href +"&response_type=token";
+  }
+
   // Search by hash tag
-  $scope.search_hashtag = '';
+  $scope.search_hashtag = 'itclub_psuti';
   $scope.searchFormSubmit = function (search_hashtag) {
-    main.images = getPhotoByTag(search_hashtag, $http);
+    main.images = getPhotosByTag($http, $scope, reload = false, search_hashtag);
   };
+
+  // AJAX loading
+  main.updatePhotos = function () {
+    // console.log("Start update");
+    // var photosBlock = document.querySelector('.photos'),
+    //     endPoint = photosBlock.offsetTop + photosBlock.offsetHeight;
+
+    // window.addEventListener('scroll', function () {
+    //   var scrollPosition = window.scrollY + window.innerHeight;
+
+    //   if (scrollPosition >= endPoint) {
+
+        // TODO: callback func
+        console.log("Loading...");
+        var newImgs = getPhotosByTag($http, $scope, reload = true, $scope.search_hashtag);
+        main.images = main.images.concat(newImgs);
+      // }
+    // });
+    // console.log("End update");
+  }
 
   // Set view mode
   main.view = '-grid';
--- src/js/engine.js
+++ src/js/engine.js
@@ -19,13 +19,12 @@ var Photo = function (id, large, thumb, caption, user, link) {
 * Get Photos By Hashtag
 * */
 
-function getPhotoByTag(tagname, $http) {
-  // Generate Tag Query
-  var query = "https://api.instagram.com/v1/tags/"+ tagname +"/media/recent?access_token="+ localStorage.accessToken + '&callback=JSON_CALLBACK';
+function instaRequest (query, $http, $scope) {
   var images = [];
-
-  // Get Data from Instagram
+  console.log("instaRequest start");
   $http.jsonp(query).success(function (response) {
+    console.log(response);
+
     // Fill array with images
     for (var i = 0; i < response.data.length; i++) {
       images[i] = new Photo(
@@ -37,64 +36,24 @@ function getPhotoByTag(tagname, $http) {
         response.data[i].link
       );
     }
+
+    $scope.instaNextUrl = response.pagination.next_max_id;
+    console.log(images);
+    console.log($scope.instaNextUrl);
   });
 
   return images;
 }
 
-/*
-* Authentication
-*   localStorage.authStep
-*   0 - need auth
-*   1 - get access token
-*   2 - access token in localStorage
-* */
+function getPhotosByTag($http, $scope, reload, tagname) {
+  // Generate Tag Query
+  var query = "https://api.instagram.com/v1/tags/"+ tagname +"/media/recent?access_token="+ localStorage.accessToken + "&callback=JSON_CALLBACK";
 
-function Auth() {
-  // Define session information in localStorage
-  localStorage.clientId = "c5004e18b7474d4cb26c1f76af8ab98e";
-
-  // Check was user authorized
-  if (localStorage.authStep != 1 && localStorage.authStep != 2) {
-    localStorage.authStep = 0;
+  if (reload === true) {
+    query += "&max_tag_id=" + $scope.instaNextUrl;
   }
 
-  if (localStorage.authStep == 0) {
-    // Update authStep
-    localStorage.authStep = 1;
-    // Redirect to Instagram auth page
-    window.location.href = "https://instagram.com/oauth/authorize/?client_id="+ localStorage.clientId +"&redirect_uri="+ window.location.href +"&response_type=token";
-  }
-  else if (localStorage.authStep == 1) {
-    // Parse access token
-    var url = window.location.href,
-      slice = "access_token=",
-      index = url.indexOf(slice);
+  console.log(query);
 
-    if (index != -1) {
-      // Get access token
-      localStorage.accessToken = url.substring(index + slice.length);
-      // Update authStep
-      localStorage.authStep = 2;
-      console.log('Auth success');
-    } else {
-      // Reset authStep
-      localStorage.authStep = 0;
-      console.log("You need auth");
-    }
-  }
-
-  console.log('Already authorized');
-  return true;
-}
-
-/*
-* Exit
-* Clear access token and reset authStep on exit
-* */
-
-function Exit() {
-  localStorage.accessToken = "";
-  localStorage.authStep = 0;
-  //return true;
+  return instaRequest(query, $http, $scope);
 }
